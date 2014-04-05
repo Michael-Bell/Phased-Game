d089adb866a3ff0f9b97730bc0b27da5b99bf1cd
Michael Bell <marvinpa97@shaw.ca>
2014-04-04 19:54:00 -0700

    Let the rain fall down

---
+++ js/rain.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Owner on 4/4/14.
+ */
+
+function loadRain(){
+    game.load.spritesheet('rain', 'assets/rain.png', 17, 17);
+}
+function createRain(){
+
+    var emitter = game.add.emitter(game.world.centerX, 0, 400);
+    emitter.width = game.world.width;
+    // emitter.angle = 30; // uncomment to set an angle for the rain.
+    emitter.makeParticles('rain');
+    emitter.minParticleScale = 0.1;
+    emitter.maxParticleScale = 0.5;
+    emitter.setYSpeed(300, 500);
+    emitter.setXSpeed(-5, 5);
+    emitter.minRotation = 0;
+    emitter.maxRotation = 0;
+    emitter.start(false, 1600, 5, 0);
+}
