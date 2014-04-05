4fb4411597276a073aecc81f4acc08a1424d37f3
Michael Bell <marvinpa97@shaw.ca>
2014-04-04 21:59:18 -0700

    Random Enemy Placement, Static Without Animations

--- js/enemy.js
+++ js/enemy.js
@@ -1,12 +1,26 @@
-var flya;
-function GenerateEnemy() {
-    enemies = game.add.group(); // using a group
-    game.physics.arcade.enable(enemies); // enemy needs physics
-    enemies.health = 5; // set the health
-    var flya = enemies.create(300, 700, 'fly'); // put in the sprite at these coordinates
-    flya.animations.add('fly', [1, 2]); // animate
 
-    flya.maxHeight = 800; // min/max height it can fly to
-    flya.minHeight = 600;
-   // flya.body.immovable = true; // Our player cannot push these guys around
+
+function GenerateEnemy(){
+
+    enemyGroup = game.add.group();
+
+    enemyGroup.enableBody = true;
+
+    enemyGroup.physicsBodyType = Phaser.Physics.ARCADE;
+
+
+
+    for (var i = 0; i < 50; i++)
+
+    {
+
+        var c = enemyGroup.create(100+i*70, game.rnd.integerInRange(0, 570), 'fly',0);
+
+        c.name = 'fly' + i;
+
+        c.body.immovable = true;
+
+    }
+
+
 }
