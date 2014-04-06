926501923bb9d2b331892f56a5ec3db8d6a872a3
flellopop <jesse_flello@hotmail.com>
2014-04-05 23:19:19 -0700

    Revert "Gold anyone?"
    This reverts commit 6134ad33101feb7a06f48a79b0a418d8bea698cf.

--- js/drops.js
+++ js/drops.js
@@ -1,22 +1,14 @@
-function collectgold(goldincrease){
+var collectgold = function(goldincrease){
     gold =+ goldincrease+Math.floor(Math.random()*LUK/5);
     
 };
 
-function gencoins(x,y){ //Trying to make a gold coin come drop out of an enemy
-    var g = coinGroup.create(x, y, 'goldcoin');
-    g.body.gravity.y = 300;
-    g.body.velocity.y = -150-Math.random()*150;
-    g.body.velocity.x = 100-Math.random()*200;
- 
-};
-
-function initCoinGroup(){
-    // Create the coin group
-    coinGroup = game.add.group();
-    coinGroup.enableBody = true;
-    coinGroup.physicsBodyType = Phaser.Physics.ARCADE;
-
-}
+/*var gencoins = function(goldincrease){ //Trying to make a gold coin come drop out of an enemy
+if(goldincrease<0){
+    
+    
+    
+}    
+};*/
 
 // TODO  add item drops
