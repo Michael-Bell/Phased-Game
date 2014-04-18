c198bf2869f5ee5c49f0aba2ab40f85def2010b0
michael-bell <marvinpa97@shaw.ca>
2014-04-17 17:26:57 -0700

    Score card on the side...

--- js/app.js
+++ js/app.js
@@ -8,3 +8,18 @@ $('.button').on('click', Foundation.utils.debounce(function(e){
     $('#level').foundation('reveal','close');
     game.state.start(game.state.current); // reset the game, should be replaced with something more reliable
 }, 300, true));
+
+function textUpdate(){
+    Foundation.utils.S('#health').html(player.health );
+    Foundation.utils.S('#gold').html(player.gold );
+    Foundation.utils.S( "#xp").html( currentxp );
+    //var node = document.getElementById('#health');
+    //node.innerHTML('<p>some dynamic html</p>');
+   /* node = document.getElementById('gold');
+    node.innerHTML('<p>some dynamic html</p>');
+    node = document.getElementById('xp');
+    node.innerHTML('<p>some dynamic html</p>');
+    Foundation.utils.S('#health').innerHTML=player.health;
+    Foundation.utils.S('#gold').innerHTML=player.gold;
+    Foundation.utils.S('#xp').innerHTML=currentxp; */
+}
