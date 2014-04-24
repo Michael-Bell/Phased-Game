7a564477780635a377be562d82e7da6f7c781b73
michael-bell <marvinpa97@shaw.ca>
2014-04-24 08:12:45 -0700

    You can only submit once now

--- js/app.js
+++ js/app.js
@@ -9,6 +9,14 @@ $('.button').on('click', Foundation.utils.debounce(function(e){
     game.state.start(game.state.current); // reset the game, should be replaced with something more reliable
 }, 300, true));
 
+
+$('#restart').on('click', Foundation.utils.debounce(function(e){
+    $('#scoreModal').foundation('reveal', 'close');
+    game.input.keyboard.disabled=false;
+    game.state.start('main'); // reset the game
+    $('#submit').removeClass('disabled');
+}, 300, true));
+
 function textUpdate(){
     Foundation.utils.S('#health').html(player.health );
     Foundation.utils.S('#gold').html(player.gold );
