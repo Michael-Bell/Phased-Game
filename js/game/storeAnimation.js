1e4452ea29ca493beb93c26ef2b5519096676591
Michael-Bell <marvinpa97@shaw.ca>
2014-05-16 11:46:39 -0700

    more store work

--- js/game/storeAnimation.js
+++ js/game/storeAnimation.js
@@ -1,10 +1,10 @@
-var anim,width;
+var anim,width,Animplayer,tilesprite;
 
 function makeAnim(){
     width = $('#storeMiddle').width();
     width=Math.floor(width/70)*70;
 
-    anim = new Phaser.Game(width, 300, Phaser.AUTO, 'animationDiv', { preload: animpreload, create: animcreate, update: animupdate });
+    anim = new Phaser.Game(width, 300, Phaser.AUTO, 'animationDiv', { preload: animpreload, create: animcreate, update: animupdate, render: testdebug});
 
 
 
@@ -16,22 +16,17 @@ function makeAnim(){
 
     }
 
-
-
-var Animplayer;
-
     function animcreate() {
 
 
         // Start the physics engine
         anim.physics.startSystem(Phaser.Physics.ARCADE);
-        anim.world.setBounds(0, 0, width+70, 300);
         //give the game gravity
         anim.physics.arcade.gravity.y = 100;
         // background color
         anim.stage.backgroundColor = 0x4488cc;
         // add player sprite
-        Animplayer = anim.add.sprite(anim.width/2, anim.height-118, 'player');
+        Animplayer = anim.add.sprite(anim.width / 2, anim.height - 118, 'player');
         // add animations
         Animplayer.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 12);
         // Enable physics on the player
@@ -39,35 +34,29 @@ var Animplayer;
         //  Animplayer.body.collideWorldBounds = true;
         Animplayer.anchor.setTo(0.5, 0.5);
         // give velocity
-        Animplayer.body.velocity.x = 200;
+        Animplayer.body.velocity.x = 100;
 
-        // make a group
-        group = anim.add.group();
 
-        group.enableBody = true;
+        tilesprite = anim.add.tileSprite(0, anim.height - 70, width, 70, 'floor');
+        anim.physics.enable(tilesprite, Phaser.Physics.ARCADE);
 
-        group.physicsBodyType = Phaser.Physics.ARCADE;
-        //math stuffs to ensure not too many blocks
-        blocks = Math.floor(anim.width / 70)+1;
+        tilesprite.autoScroll(-140,0);
+        tilesprite.body.immovable = true;
 
-        for (i = -1; i <= blocks; i++) {
-            var b = group.create(i * 70, anim.height - 70, 'floor');
-            b.body.allowGravity = false;
-            b.body.immovable = true;
-            b.body.velocity.x = -200;
+        tilesprite.body.allowGravity = false;
 
-            // console.log(b);
-        }
+
 
     }
 
     function animupdate() {
-        anim.physics.arcade.collide(Animplayer, group);
+
+        anim.physics.arcade.collide(Animplayer, tilesprite);
         Animplayer.animations.play('walk');
-        anim.physics.arcade.collide(group, group);
-        group.forEach(function (item) {
-            anim.world.wrap(item,69);
-        });
-        anim.world.wrap(Animplayer);
+        Animplayer.x=anim.width/2;
+    }
+    function testdebug(){
+        anim.debug.spriteInfo(Animplayer, 0, 32);
+
     }
 }
