9937f4f85e15f7111709b2938c0b0dbacd65a807
michael-bell <marvinpa97@shaw.ca>
2014-05-21 16:24:45 -0700

    more store work

--- js/game/laser.js
+++ js/game/laser.js
@@ -6,4 +6,5 @@ var RAmmo={};
 RAmmo.SHOT_DELAY = 500; // milliseconds (10 bullets/second)
 RAmmo.BULLET_SPEED = 250; // pixels/second
 RAmmo.NUMBER_OF_BULLETS = 10;
+RAmmo.life=0;
 RAmmo.enabled=false;
--- js/game/store.js
+++ js/game/store.js
@@ -23,8 +23,11 @@ function updateStore(){
     $("#StoreXp").text(currentxp);
     $("#StoreGold").text(player.gold);
     $("#xpBox").text(currentxp+'/'+xpneeded);
-
     $('#xpMeter').css('width', currentxp/xpneeded*100 + '%');
+    $('#QLifespan').text(bulletLifespan/1000);
+    $('#WLifespan').text(WAmmo.missileLifespan/1000);
+    $('#ELifespan').text(EAmmo.missileLifespan/1000);
+    $('#RLifespan').text(RAmmo.life/1000);
 }
 
 $('#CloseStore').on('click', Foundation.utils.debounce(function(e){
@@ -39,7 +42,22 @@ $('#CloseStore').on('click', Foundation.utils.debounce(function(e){
     pauseAni();
 }, 300, true));
 
-
+/* New Gun Buttons*/
 $('#enableTNT').on('click',Foundation.utils.debounce(function(e){EAmmo.enabled=true;},true));
 $('#enableMissile').on('click',Foundation.utils.debounce(function(e){WAmmo.enabled=true;},true));
 $('#enableLaser').on('click',Foundation.utils.debounce(function(e){RAmmo.enabled=true;},true));
+
+/* Lifespan Buttons */
+$('#QLifeM').on('click',Foundation.utils.debounce(function(e){bulletLifespan-=500; updateStore();},true));
+$('#QLifeA').on('click',Foundation.utils.debounce(function(e){bulletLifespan+=500; updateStore();},true));
+
+$('#WLifeM').on('click',Foundation.utils.debounce(function(e){WAmmo.missileLifespan-=500; updateStore();},true));
+$('#WLifeA').on('click',Foundation.utils.debounce(function(e){WAmmo.missileLifespan+=500; updateStore();},true));
+
+$('#ELifeM').on('click',Foundation.utils.debounce(function(e){EAmmo.missileLifespan-=500; updateStore();},true));
+$('#ELifeA').on('click',Foundation.utils.debounce(function(e){EAmmo.missileLifespan+=500; updateStore();},true));
+
+$('#RLifeM').on('click',Foundation.utils.debounce(function(e){EAmmo.missileLifespan-=500; updateStore();},true));
+$('#RLifeA').on('click',Foundation.utils.debounce(function(e){EAmmo.missileLifespan+=500; updateStore();},true));
+
+
